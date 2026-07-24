c819629dfaaafacf6a015f669689a950e738e539
ppbshiniba <1012004041@qq.com>
2026-07-24 11:51:05 +0800

    Fix image paths and clean up code formatting

--- python.py
+++ python.py
@@ -11,8 +11,8 @@ st.write("qio是一种类人猿，由于其温顺的性格被誉为人类最好�
 st.write("它们体型较大，长大后可到达100kg~150kg不等，被誉为类人猿界的“良子”")
 st.write("它在最早期时由于同类之间存在生殖隔离，选择与猿类联姻，以下是珍贵图片")
 
-st.image("./tu/b.jpg")
+st.image("b.jpg")
 
 st.write("由于当时人类并没有发明出雨伞，所以也是有了这么一张令人看了怜爱的照片")
-st.image("./tu/c.jpg")
+st.image("c.jpg")
 
