05cb8c98e6158735dbb9aa23fb0555f07a588e71
ppbshiniba <1012004041@qq.com>
2026-07-24 14:52:48 +0800

    Update python.py

--- python.py
+++ python.py
@@ -1,5 +1,17 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="拯救者联盟简介",
+    page_icon="🧊",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Get Help': 'https://7921.e73k.com/enter/index.html',
+        'Report a bug': "https://7921.e73k.com/enter/index.html",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
 #标题
 st.title("突脸")
 
@@ -15,4 +27,9 @@ st.image("b.jpg")
 
 st.write("由于当时人类并没有发明出雨伞，所以也是有了这么一张令人看了怜爱的照片")
 st.image("c.jpg")
+
+st.write("四人组一般有一个逗比")
+st.write("卢康，网名落阔，自诩为“艾希迪克”，此人爱好健身，但是有健身人士的通病，就是头脑简单四肢发达")
+st.write("一次，由于第一次发现QQ有神奇的特效，所以高兴得吐出了舌条，这是珍贵记录")
+st.image("d.jpg")
 
